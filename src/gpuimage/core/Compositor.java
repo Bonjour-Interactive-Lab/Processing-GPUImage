f4e4353145015f66c55a7a6d59f865f87fda8a0e
ALEX-MSI\bonjour <alex@bonjour-lab.com>
2018-07-13 18:11:00 +0200

    update Compositor javadoc

--- src/gpuimage/core/Compositor.java
+++ src/gpuimage/core/Compositor.java
@@ -63,114 +63,311 @@ public class Compositor extends GPUImageBaseEffects{
 	 * 
 	 * 
 	 ...............................................*/
+	/**
+	 * Additive blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendAddImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.ADD);
 	}
 	
+	/**
+	 * Average blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendAverageImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.AVERAGE);
 	}
-
+	
+	/**
+	 * Color blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendColorImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.COLOR);
 	}
-
+	
+	/**
+	 * Color burn blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendColorBurnImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.COLORBURN);
 	}
-
+	
+	/**
+	 * Color dodge blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendColorDodgeImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.COLORDODGE);
 	}
-
+	
+	/**
+	 * Darken blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendDarkenImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.DARKEN);
 	}
-
+	
+	/**
+	 * Diffrence blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendDifferenceImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.DIFFERENCE);
 	}
-
+	
+	/**
+	 * Exclusion blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendExclusionImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.EXCLUSION);
 	}
-
+	
+	/**
+	 * Glow blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendGlowImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.GLOW);
 	}
-
+	
+	/**
+	 * Hard light blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendHardLightImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.HARDLIGHT);
 	}
-
+	
+	/**
+	 * Hard mix blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendHardMixImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.HARDMIX);
 	}
-
+	
+	/**
+	 * Hue blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendHueImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.HUE);
 	}
-
+	
+	/**
+	 * Lighten blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendLightenImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.LIGHTEN);
 	}
-
+	
+	/**
+	 * Color blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendLinearBurnImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.LINEARBURN);
 	}
-
+	
+	/**
+	 * Linear dodge blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendLinearDodgeImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.LINEARDODGE);
 	}
-
+	
+	/**
+	 * Linear light blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendLinearLightImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.LINEARLIGHT);
 	}
-
+	
+	/**
+	 * Luminosity blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendLuminosityImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.LUMINOSITY);
 	}
-
+	
+	/**
+	 * Multiply blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendMultiplyImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.MULTIPLY);
 	}
-
+	
+	/**
+	 * Negation blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendNegationImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.NEGATION);
 	}
-
+	
+	/**
+	 * Phoenix blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendPhoenixImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.PHOENIX);
 	}
-
+	
+	/**
+	 * Pin light blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendPinLightImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.PINLIGHT);
 	}
-
+	
+	/**
+	 * Reflect blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendReflectImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.REFLECT);
 	}
-
+	
+	/**
+	 * Saturation blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendSaturationImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.SATURATION);
 	}
-
+	
+	/**
+	 * Screen blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendScreenImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.SCREEN);
 	}
-
+	
+	/**
+	 * Soft light blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendSoftLightImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.SOFTLIGHT);
 	}
-
+	
+	/**
+	 * Substract blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendSubstractImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.SUBSTRACT);
 	}
-
+	
+	/**
+	 * Vividlight blending
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @return
+	 */
 	public PGraphics getBlendVividLightImage(PImage src, PImage base, float opacity) {
 		return getBlendImage(src, base, opacity, GPUImageInterface.VIVIDLIGHT);
 	}
 	
+	/**
+	 * Blend image
+	 * @param src source layer to blend
+	 * @param base base layer
+	 * @param opacity opacity of the src blended layer on the base layer between 0/100
+	 * @param NAME Blending name
+	 * @return
+	 */
 	public PGraphics getBlendImage(PImage src, PImage base, float opacity, String NAME) {
 		super.setCurrentSH(NAME);
 		super.currentSH.set("base", base);
