e227cc1cb1fdc746146f4010d35894c1c7d74247
ALEX-MSI\bonjour <alex@bonjour-lab.com>
2018-07-13 16:50:39 +0200

    add ProceduralTexture Pattern

---
+++ src/gpuimage/core/ProceduralTexture.java
@@ -0,0 +1,14 @@
+package gpuimage.core;
+
+import processing.core.*;
+import processing.opengl.*;
+/**
+ * Filtering class wich provide various postFX and filtering methods usefull for design, compositing or computervision.
+ * Each available shaders are define in the GPUImageInterface.
+ * @see #GPUImageInterface
+ * 
+ * @author bonjour
+ */
+public class ProceduralTexture extends GPUImageBaseEffects{
+	
+}
