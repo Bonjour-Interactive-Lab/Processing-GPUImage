50b18ebf888286c190bb6cd6faf595f0ad305934
ALEX-MSI\bonjour <alex@bonjour-lab.com>
2018-07-26 16:11:46 +0200

    add kinect raw example

---
+++ examples/others/KinectRawDepthIntoRGBAMod/data/frag.glsl
@@ -0,0 +1,63 @@
+#version 150
+#ifdef GL_ES
+precision highp float;
+precision highp vec4;
+precision highp vec3;
+precision highp vec2;
+precision highp int;
+#endif
+
+uniform sampler2D dataTexture;
+uniform int dataMax;
+uniform float time;
+
+in vec4 vertTexCoord;
+out vec4 fragColor;
+
+
+vec4 encodeRGBAMod(float value, float edge){
+	float divider = float(edge) / 256.0;
+
+	float modValue = mod(value, 255.0);
+	float modIndex = value / 255.0;
+
+	float index = modIndex / divider;
+	float luma = fract(modValue);
+
+	return vec4(luma, luma, index, 1.0);
+}
+
+float decodeRGBAMod(vec4 rgba, float edge){
+	float divider = float(edge) / 256.0;
+	float index = round(rgba.b * divider);
+
+	return rgba.r + 255 * index;
+}
+
+void main(){
+	vec2 uv = vertTexCoord.xy;
+	vec4 tex = texture2D(dataTexture, uv);
+
+	float data = decodeRGBAMod(tex, dataMax) / float(dataMax);
+	//vec4 encodedData = encodeRGBAMod(data, float(dataMax));
+
+	float distanceTex = 1.0 - smoothstep(0.0, 1.0, data);
+	float target = fract(time);	
+	vec3 DepthSlice = vec3(0.0);
+	#define iteration 2
+	float smoothing = 0.25 / float(iteration);
+	float thickness = 0.5 / float(iteration);
+	for(int i=0; i<iteration; i++){
+		float fi = float(i) / float(iteration);
+		float targeti = fract(fi + target) * pow(distanceTex, 0.05);
+		//targeti = clamp(targeti, 0.0, 0.979);
+		float df1 = smoothstep((targeti - thickness * 0.5) - smoothing, (targeti - thickness * 0.5) + smoothing, 1.0 - distanceTex);
+		float df2 = smoothstep((targeti + thickness * 0.5) - smoothing, (targeti + thickness * 0.5) + smoothing, 1.0 - distanceTex);
+		float df = df1 - df2;
+		DepthSlice += vec3(df);
+	}
+	vec3 RGB = mix(vec3(0.1, 0.0, 0.0), vec3(0.0, 1.0, 1.0), DepthSlice.r) * distanceTex;
+	vec4 RGBA = vec4(RGB + vec3(0.05, 0.0, 0.25), 1.0) * 1.25;
+
+	fragColor = RGBA;
+}
